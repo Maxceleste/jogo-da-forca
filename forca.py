1819bae402794540ed361cf44bd54f3b677e4651
Maxceleste <maxpauloteixeira@gmail.com>
2022-05-17 21:41:42 -0300

    Criando algumas definições e iniciando a classe

---
+++ forca.py
@@ -0,0 +1,21 @@
+'''
+Primeiramente vamos dividir o jogo da forca em funções menores:
+- Gerar a palavra aleatória
+- Esconder a palavra do jogador
+- mapear as letras da palavra e suas respectivas posições
+- checar se a letra existe ou não, retornando True ou False
+- Se True, preencher os espaços vazios
+- Se False, Perder uma das 6 chances
+- Perder se acabar as chances, ganhar se acertar todas as letras
+
+Posteriormente adicionar um personagem igual o jogo da forca clássico
+'''
+
+class Jogar():
+
+    palavras = (
+        'planeta',
+        'cidade',
+        'carro',
+        'animal'
+    )
